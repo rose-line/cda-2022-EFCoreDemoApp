c6f108b160417a908a7447f8e6f2044b8321eed0
pgahide <patrice.gahide@gmail.com>
2022-01-05 09:33:53 +0100

    séance 05/01/2021 : données en relation, quelques exemples

--- EFCoreDemoApp.ConsoleUI/Program.cs
+++ EFCoreDemoApp.ConsoleUI/Program.cs
@@ -2,6 +2,7 @@
 using EFCoreDemoApp.Donnees;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace EFCoreDemoApp.ConsoleUI
@@ -85,6 +86,102 @@ namespace EFCoreDemoApp.ConsoleUI
       {
         _context.Acteurs.Add(new Acteur { Nom = nom });
       }
+      _context.SaveChanges();
+    }
+
+    private static void RecupEtSupprimerActeur()
+    {
+      var acteur = _context.Acteurs.Find(3);
+      _context.Acteurs.Remove(acteur);
+      _context.SaveChanges();
+    }
+
+    private static void InsererRelation()
+    {
+      var acteur = new Acteur
+      {
+        Nom = "Arnold Schwarzenegger",
+        Citations = new List<Citation>
+        {
+          new Citation { Texte = "I'll be back" },
+          new Citation { Texte = "Sarah Connor?" }
+        }
+      };
+      _context.Acteurs.Add(acteur);
+      _context.SaveChanges();
+    }
+
+    private static void InsererRelationAvecActeurExistant()
+    {
+      var acteur = _context.Acteurs.FirstOrDefault();
+      acteur.Citations.Add(
+        new Citation
+        {
+          Texte = "Hello!"
+        });
+      _context.SaveChanges();
+    }
+
+    private static void EagerLoading()
+    {
+      var acteursEtCitations = _context.Acteurs.Include(a => a.Citations).ToList();
+      var enSplitQuery = _context.Acteurs.AsSplitQuery().Include(a => a.Citations).ToList();
+      var avecFiltre = _context.Acteurs.Include(a => a.Citations.Where(c => c.Texte.Contains("Hello"))).ToList();
+      var depuisUnParentSpecifique =
+        _context.Acteurs.Where(a => a.Nom.Contains("Schwarz"))
+                        .Include(a => a.Citations).FirstOrDefault();
+
+      // Enfants et petits enfants
+      //var acteursEtCitationsEtTraductions = _context.Acteurs.Include(a => a.Citations).ThenInclude(c => c.Traductions).ToList();
+
+      // Petits enfants seulement
+      //var acteursEtTraductions = _context.Acteurs.Include(a => a.Citations.Traductions).ToList();
+
+      // Plusieurs catégories d'enfants
+      //var acteursEtCitationsEtVoitures = _context.Acteurs.Include(a => a.Citations).Include(a => a.Voitures).ToList();
+    }
+
+    private static void Projection()
+    {
+      // avec type anonyme
+      var desProprietes = _context.Acteurs.Select(a => new { a.Id, a.Nom }).ToList();
+      // avec type existant
+      var idEtNoms = _context.Acteurs.Select(a => new IdEtNom(a.Id, a.Nom)).ToList();
+    }
+
+    public struct IdEtNom
+    {
+      public IdEtNom(int id, string nom)
+      {
+        Id = id;
+        Nom = nom;
+      }
+
+      public int Id { get; }  
+      public string Nom { get; }  
+
+    }
+
+    private static void LazyLoading()
+    {
+      var acteur = _context.Acteurs.Find(2);
+      // Ne fonctionne que si Lazy Loading est activé explicitement
+      var compteCitations = acteur.Citations.Count();
+    }
+
+    private static void FiltrageDeDonneeEnRelation()
+    {
+      var acteurs = _context.Acteurs
+                            .Where(a => a.Citations.Any(c => c.Texte.Contains("hello")))
+                            .ToList();
+    }
+
+    private static void UpdateDonneesEnRelation()
+    {
+      var acteur = _context.Acteurs.Include(a => a.Citations)
+                                   .FirstOrDefault(a => a.Id == 2);
+      acteur.Citations[0].Texte = "Wrong.";
+      _context.Citations.Remove(acteur.Citations[1]);
       _context.SaveChanges();
     }
 
